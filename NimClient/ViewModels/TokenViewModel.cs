116ec02ca7c6601bbee08f1d7eced3fd67af88bc
Adam <rareaves@live.com>
2014-12-30 15:24:00 -0500

    merged from tokenized

---
+++ NimClient/ViewModels/TokenViewModel.cs
@@ -0,0 +1,23 @@
+﻿using NimClient.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimClient.ViewModels
+{
+    internal class TokenViewModel : BaseViewModel
+    {
+        private RelayCommand _tokentap;
+
+        public RelayCommand TokenTap { get { return _tokentap; } }
+
+        public TokenViewModel() : this(new RelayCommand((obj) => { return; })) { }
+
+        public TokenViewModel(RelayCommand tokentap)
+        {
+            _tokentap = tokentap;
+        }
+    }
+}
